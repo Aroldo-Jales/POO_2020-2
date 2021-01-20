0c3929937246eeda21e4f56eb6feee149e7ae3e0
Aroldo Jales <48775971+Aroldo-Jales@users.noreply.github.com>
2021-01-20 12:11:22 -0300

    Create Hora.java

---
+++ Atividades/POO_ATV_6_PT2/questao_05/Hora.java
@@ -0,0 +1,43 @@
+package ATV_05;
+
+public class Hora {
+    private int hora;
+    private int minutos;
+    private int segundos;
+
+    public Hora(int hora, int minutos, int segundos) {
+        this.hora = hora;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+    
+    public void imprimeHora(){
+        System.out.print(this.getHora()+":");
+        System.out.print(this.getMinutos()+":");
+        System.out.print(this.getSegundos());
+    }
+    
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
+    }
+}
