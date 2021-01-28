8b9bc0988d3d06b575a07a8ce4962b8309f63d32
Aroldo Jales <48775971+Aroldo-Jales@users.noreply.github.com>
2021-01-27 23:59:34 -0300

    Create Exceptions.java

---
+++ Atividades/POO_ATV_7/questao_10/Exceptions.java
@@ -0,0 +1,19 @@
+package ATV_07;
+import java.util.Scanner;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String palavra = null;
+        
+        try {
+            palavra = 1;
+        } catch (NullPointerException e) {
+            System.out.println("String nula!");
+        } catch (Exception e) {
+            System.out.println("Ocorreu alguma excessao!");
+        } finally {
+            System.out.println("Finally!");
+        }  
+    } 
+}
